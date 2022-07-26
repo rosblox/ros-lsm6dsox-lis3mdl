2d47db806950670816a08f7f97754509e85f6977
Max <max@seervision.com>
2022-07-26 17:12:50 +0200

    Publish also magnetometer.

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ ARG ROS_DISTRO
 FROM ros:${ROS_DISTRO}-ros-core
 
 RUN apt update && apt install -y --no-install-recommends python3-rpi.gpio python3-pip python3-colcon-common-extensions && rm -rf /var/lib/apt/lists/*
-RUN pip3 install adafruit-circuitpython-lsm6ds
+RUN pip3 install adafruit-circuitpython-lsm6ds adafruit-circuitpython-lis3mdl
 
 
 WORKDIR /colcon_ws/src
--- ros_lsm6ds/ros_lsm6ds/publisher_member_function.py
+++ ros_lsm6ds/ros_lsm6ds/publisher_member_function.py
@@ -14,51 +14,64 @@
 
 import rclpy
 from rclpy.node import Node
-from sensor_msgs.msg import Imu
+from sensor_msgs.msg import Imu, MagneticField
 
 
 import board
-from adafruit_lsm6ds.lsm6dsox import LSM6DSOX
 
-class RosLsm6dsPublisher(Node):
+import adafruit_lsm6ds
+import adafruit_lis3mdl
+
+
+class RosLsm6dsoxLis3mdlPublisher(Node):
 
     def __init__(self):
-        super().__init__('ros_lsm6ds_publisher')
+        super().__init__('ros_lsm6dsox_lis3mdl_publisher')
         self.i2c = board.I2C()
-        self.lsm6ds = LSM6DSOX(self.i2c)
+        self.lsm6dsox = adafruit_lsm6ds.LSM6DSOX(self.i2c)
+        self.lis3mdl = adafruit_lis3mdl.LIS3MDL(self.i2c)
 
-        self.publisher_ = self.create_publisher(Imu, '/imu/data_raw', 10)
+        self.lsm6dsox_publisher = self.create_publisher(Imu, '/imu/data_raw', 10)
+        self.lis3mdl_publisher = self.create_publisher(MagneticField, '/imu/mag', 10)
         timer_period = 0.1  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
 
     def timer_callback(self):
-        acc_x, acc_y, acc_z = self.lsm6ds.acceleration
-        rot_x, rot_y, rot_z = self.lsm6ds.gyro
+        acc_x, acc_y, acc_z = self.lsm6dsox.acceleration
+        rot_x, rot_y, rot_z = self.lsm6dsox.gyro
+        mag_x, mag_y, mag_z = self.lis3mdl.magnetic
 
-        msg = Imu()
-        msg.header.stamp = self.get_clock().now().to_msg()
-        msg.header.frame_id = "ros_lsm6ds"
-        msg.linear_acceleration.x = acc_x
-        msg.linear_acceleration.y = acc_y
-        msg.linear_acceleration.z = acc_z
-        msg.angular_velocity.x = rot_x
-        msg.angular_velocity.y = rot_y
-        msg.angular_velocity.z = rot_z
+        lsm6dsox_msg = Imu()
+        lsm6dsox_msg.header.stamp = self.get_clock().now().to_msg()
+        lsm6dsox_msg.header.frame_id = "ros_lsm6dsox"
+        lsm6dsox_msg.linear_acceleration.x = acc_x
+        lsm6dsox_msg.linear_acceleration.y = acc_y
+        lsm6dsox_msg.linear_acceleration.z = acc_z
+        lsm6dsox_msg.angular_velocity.x = rot_x
+        lsm6dsox_msg.angular_velocity.y = rot_y
+        lsm6dsox_msg.angular_velocity.z = rot_z
 
-        self.publisher_.publish(msg)
+        lis3mdl_msg = MagneticField()
+        lis3mdl_msg.header = lsm6dsox_msg.header
+        lis3mdl_msg.magnetic_field.x = mag_x
+        lis3mdl_msg.magnetic_field.y = mag_y
+        lis3mdl_msg.magnetic_field.z = mag_z
+        
+        self.lsm6dsox_publisher.publish(lsm6dsox_msg)
+        self.lis3mdl_publisher.publish(lis3mdl_msg)
 
 
 def main(args=None):
     rclpy.init(args=args)
 
-    ros_lsm6ds_publisher = RosLsm6dsPublisher()
+    ros_lsm6dsox_lis3mdl_publisher = RosLsm6dsoxLis3mdlPublisher()
 
-    rclpy.spin(ros_lsm6ds_publisher)
+    rclpy.spin(ros_lsm6dsox_lis3mdl_publisher)
 
     # Destroy the node explicitly
     # (optional - otherwise it will be done automatically
     # when the garbage collector destroys the node object)
-    ros_lsm6ds_publisher.destroy_node()
+    ros_lsm6dsox_lis3mdl_publisher.destroy_node()
     rclpy.shutdown()
 
 
