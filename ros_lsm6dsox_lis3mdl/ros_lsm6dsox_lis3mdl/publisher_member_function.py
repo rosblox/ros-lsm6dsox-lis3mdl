b7d09c023a276f4a38fea518e267cfed07ac6359
Max Polzin <polzin.max@gmail.com>
2023-08-04 12:39:11 +0200

    Update publisher_member_function.py

--- ros_lsm6dsox_lis3mdl/ros_lsm6dsox_lis3mdl/publisher_member_function.py
+++ ros_lsm6dsox_lis3mdl/ros_lsm6dsox_lis3mdl/publisher_member_function.py
@@ -33,7 +33,7 @@ class RosLsm6dsoxLis3mdlPublisher(Node):
 
         self.lsm6dsox_publisher = self.create_publisher(Imu, '/imu/data_raw', 10)
         self.lis3mdl_publisher = self.create_publisher(MagneticField, '/imu/mag', 10)
-        timer_period = 0.02  # seconds
+        timer_period = 0.05  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
 
     def timer_callback(self):
