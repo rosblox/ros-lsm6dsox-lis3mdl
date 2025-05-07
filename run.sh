492d7c6324a109aa255ec97cd10e83a6396f2ffd
Max Polzin <polzin.max@gmail.com>
2025-05-07 18:54:46 +0200

    Update to new style.

--- run.sh
+++ run.sh
@@ -2,11 +2,10 @@
 
 REPOSITORY_NAME="$(basename "$(dirname -- "$( readlink -f -- "$0"; )")")"
 
-docker run -it --rm \
---network=host \
---ipc=host --pid=host \
---privileged \
---env UID=$(id -u) \
---env GID=$(id -g) \
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+export HOST_UID=$(id -u)
+
+docker compose -f $SCRIPT_DIR/docker-compose.yml run \
 --volume ./ros_lsm6dsox_lis3mdl:/colcon_ws/src/ros_lsm6dsox_lis3mdl \
-ghcr.io/rosblox/${REPOSITORY_NAME}:humble
+${REPOSITORY_NAME} bash
