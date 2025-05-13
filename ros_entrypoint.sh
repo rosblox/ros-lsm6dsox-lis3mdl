18999018c467671292fd54af55d70b38b11e7c30
Max Polzin <polzin.max@gmail.com>
2025-05-13 17:04:39 +0200

    Update.

--- ros_entrypoint.sh
+++ ros_entrypoint.sh
@@ -6,6 +6,9 @@ id -u ros &>/dev/null || adduser --quiet --disabled-password --gecos '' --uid ${
 getent group i2c || groupadd -g 998 i2c
 usermod -aG i2c ros
 
+getent group gpio || groupadd -g 997 gpio
+usermod -aG gpio ros
+
 source /opt/ros/${ROS_DISTRO}/setup.bash
 source /colcon_ws/install/setup.bash
 
